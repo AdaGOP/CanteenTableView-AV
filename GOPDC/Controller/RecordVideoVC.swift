7e3928b55289685ca665c15405e96c31a3154f7f
David Gunawan <david_gunawan_@hotmail.com>
2020-05-13 22:55:08 +0700

    Fix Save to Album bug

--- GOPDC/Controller/RecordVideoVC.swift
+++ GOPDC/Controller/RecordVideoVC.swift
@@ -11,6 +11,8 @@ import MobileCoreServices
 
 class RecordVideoVC: UIViewController, UINavigationControllerDelegate {
     
+    let videoFileName = "/video.mp4"
+    
     @IBAction func recordTapped(_ sender: AnyObject) {
         VideoHelper.startMediaBrowser(delegate: self, sourceType: .camera)
     }
@@ -26,16 +28,19 @@ class RecordVideoVC: UIViewController, UINavigationControllerDelegate {
 }
 
 extension RecordVideoVC: UIImagePickerControllerDelegate {
-    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedVideo:URL = (info[UIImagePickerController.InfoKey(rawValue: UIImagePickerController.InfoKey.mediaURL.rawValue)] as? URL) {
+            // Save video to the main photo album
+            UISaveVideoAtPathToSavedPhotosAlbum(selectedVideo.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
+            
+            // Save the video to the app directory so we can play it later
+            let videoData = try? Data(contentsOf: selectedVideo)
+            let paths = NSSearchPathForDirectoriesInDomains(
+                FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+            let documentsDirectory: URL = URL(fileURLWithPath: paths[0])
+            let dataPath = documentsDirectory.appendingPathComponent(videoFileName)
+            try! videoData?.write(to: dataPath, options: [])
+        }
         dismiss(animated: true, completion: nil)
-      
-        guard let mediaType = info[UIImagePickerController.InfoKey.mediaType.rawValue] as? String,
-        mediaType == (kUTTypeMovie as String),
-            let url = info[UIImagePickerController.InfoKey.mediaURL.rawValue] as? URL,
-        UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path)
-        else { return }
-      
-      // Handle a movie capture
-      UISaveVideoAtPathToSavedPhotosAlbum(url.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
     }
 }
