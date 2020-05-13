018503f785d40001b735c91608c45470e5c4ec4f
David Gunawan <david_gunawan_@hotmail.com>
2020-05-13 19:54:44 +0700

    Add Gesture Tap in PlayerView

--- GOPDC/MediaPlaybackVC.swift
+++ GOPDC/MediaPlaybackVC.swift
@@ -13,20 +13,45 @@ import AVFoundation
 class MediaPlaybackVC: UIViewController {
 
     @IBOutlet weak var videoPlayer: VideoView!
+    @IBOutlet weak var controlLayerView: UIView!
     @IBOutlet weak var playPauseButton: UIButton!
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupVideo()
-        view.sendSubviewToBack(videoPlayer)
+        
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupVideo()
+        resetTimer()
+        addGesture()
+    }
+    
     func setupVideo() {
         let url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
         videoPlayer.configure(url: url)
         videoPlayer.play()
-        
+    }
+    
+    func resetTimer() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 10.0, target: self, selector: #selector(hideControls), userInfo: nil, repeats: false)
+    }
+    
+    func addGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(toggleControls))
+        controlLayerView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func hideControls() {
+        playPauseButton.isHidden = true
+    }
+    
+    @objc func toggleControls() {
+        playPauseButton.isHidden = !playPauseButton.isHidden
+        resetTimer()
     }
     
     @IBAction func playPauseTapped(_ sender: UIButton) {
