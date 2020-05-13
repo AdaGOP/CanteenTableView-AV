cf97598e41c1f5d5b2c40d97213132b03f933ee8
David Gunawan <david_gunawan_@hotmail.com>
2020-05-13 22:28:18 +0700

    Add Video Recording Button

---
+++ GOPDC/Controller/RecordVideoVC.swift
@@ -0,0 +1,30 @@
+//
+//  RecordVideoVC.swift
+//  GOPDC
+//
+//  Created by David Gunawan on 13/05/20.
+//  Copyright © 2020 David Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class RecordVideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
